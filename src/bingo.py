59a80683358a74b519efcf82efb9109872a035cc
John Sizemore <size@Johns-MacBook-Pro.local>
2020-04-01 15:25:57 -0600

    ready for Wed

--- src/bingo.py
+++ src/bingo.py
@@ -2,6 +2,7 @@ from werds import werds
 import numpy as np 
 
 if __name__ == '__main__':
-    card = np.random.choice(werds, size=[5,5], replace=False)
+    card = np.random.choice(set(werds), size=[5,5], replace=False)
     card[2,2] = 'FREE'
     print(card)
+    
